50fd8a8bb20c6cbf6a7bfde6d0227c1ed28f4d7e
swastik959 <Sswastik959@gmail.com>
2024-03-29 20:13:22 +0530

    added the c and the main go code
    Signed-off-by: swastik959 <Sswastik959@gmail.com>

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+)
+
+/*
+#cgo CFLAGS: -I/path/to/c/headers
+#cgo LDFLAGS: -L/path/to/c/libraries -lmy_c_library
+#include "my_c_library.h"
+*/
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatalf("Usage: %s <interface> <port>\n", os.Args[0])
+	}
+
+	ifaceName := os.Args[1]
+	iface, _ := net.InterfaceByName(ifaceName)
+
+	portStr := os.Args[2]
+	port, err := strconv.Atoi(portStr)
+	if err != nil || port <= 0 || port > 65535 {
+		log.Fatalf("Invalid port number: %s\n", portStr)
+	}
+
+	specs, err := ebpf.LoadCollectionSpec("xdp_prog.o")
+	if err != nil {
+		log.Fatalf("Failed to load ebpf collection spec: %v", err)
+	}
+
+	coll, err := ebpf.NewCollection(specs)
+	if err != nil {
+		log.Fatalf("Failed to load eBPF collection: %v", err)
+	}
+	defer coll.Close()
+
+	prog, ok := coll.Programs["xdp_prog"]
+	if !ok {
+		log.Fatal("Cannot find XDP program in collection")
+	}
+
+	bpfMap, ok := coll.Maps["port_map"]
+	if !ok {
+		log.Fatal("Cannot find map in collection")
+	}
+
+	link, err := link.AttachXDP(link.XDPOptions{
+		Program:   prog,
+		Interface: iface.Index,
+	})
+	if err != nil {
+		log.Fatalf("Failed to attach XDP program: %v", err)
+	}
+	defer link.Close()
+
+	if err := bpfMap.Put(uint32(0), uint32(port)); err != nil {
+		log.Fatalf("Failed to update map: %v", err)
+	}
+
+	fmt.Printf("XDP program successfully attached. Filtering port %d on %s\n", port, iface)
+
+}
---
+++ xdp_prog.c
@@ -0,0 +1,59 @@
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/tcp.h>
+
+struct bpf_map_def SEC("maps") port_map = {
+    .type        = BPF_MAP_TYPE_ARRAY,
+    .key_size    = sizeof(__u32),
+    .value_size  = sizeof(__u32),
+    .max_entries = 1,
+};
+
+SEC("xdp_prog")
+int xdp_drop_port(struct xdp_md *ctx) {
+    void *data_end = (void *)(long)ctx->data_end;
+    void *data = (void *)(long)ctx->data;
+
+    // Ethernet header check
+    struct ethhdr *eth = data;
+    if ((void *)(eth + 1) > data_end) {
+        return XDP_PASS;
+    }
+
+    // IPv4 check
+    if (eth->h_proto != htons(ETH_P_IP)) {
+        return XDP_PASS;
+    }
+
+    // IP header check
+    struct iphdr *ip = data + sizeof(*eth);
+    if ((void *)(ip + 1) > data_end) {
+        return XDP_PASS;
+    }
+
+    // TCP check
+    if (ip->protocol != IPPROTO_TCP) {
+        return XDP_PASS;
+    }
+
+    // TCP header check
+    struct tcphdr *tcp = (void *)ip + ip->ihl * 4;
+    if ((void *)(tcp + 1) > data_end) {
+        return XDP_PASS;
+    }
+
+    // Retrieve port from map (if it exists)
+    __u32 key = 0;
+    __u32 *port = bpf_map_lookup_elem(&port_map, &key);
+    if (!port) {
+        return XDP_PASS; // No port in map, pass traffic
+    }
+
+    // Drop if destination port matches
+    if (tcp->dest == htons(*port)) {
+        return XDP_DROP;
+    }
+
+    return XDP_PASS;
+}
